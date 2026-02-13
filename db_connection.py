c1c9a56436718dc93af4d17cdd5f6b17e83ce37a
NaveenPrabodha <naveenprabodha3@gmail.com>
2026-02-14 03:40:40 +0530

    first commit

---
+++ db_connection.py
@@ -0,0 +1,28 @@
+import mysql.connector
+
+con = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="nav123@admin",
+    database="emp"
+)
+
+def check_employee(employee_id):
+    # Query to select all rows from the employees table where id matches
+    sql = 'SELECT * FROM employees WHERE id=%s'
+
+    # Making cursor buffered to make rowcount method work properly
+    cursor = con.cursor(buffered=True)
+    data = (employee_id,)
+
+    # Executing the SQL Query
+    cursor.execute(sql, data)
+
+    # Fetch the first row to check if employee exists
+    employee = cursor.fetchone()
+
+    # Closing the cursor
+    cursor.close()
+
+    # If employee is found, return True, else return False
+    return employee is not None
